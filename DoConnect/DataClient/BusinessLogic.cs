ef6a9e37a3935c958d4cae4cae09f81338fdee6e
Thabane Ndaba <skinnybonafide@yahoo.com>
2016-06-15 22:36:04 +0200

    Data Access Global class

--- DoConnect/DataClient/BusinessLogic.cs
+++ DoConnect/DataClient/BusinessLogic.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +11,24 @@ namespace DataClient
 {
     public class BusinessLogic
     {
-
+        private string Conn = string.Empty;
+        public BusinessLogic()
+        {
+            Conn = ConfigurationManager.ConnectionStrings["DB"].ConnectionString;
+        }
+        public int CreateUser()
+        {
+            DataAccess da = new DataAccess();
+            da.ExecuteNonQuery(Conn,new List<SqlParameter>(),"[CreateUser]");
+            int userId = 0;
+            using (var reader = da.ExecuteReader(Conn, "[CreateUser]", new List<SqlParameter>()))
+            {
+                if (reader.Read())
+                {
+                   userId = reader.GetInt32(reader.GetOrdinal("ID"));
+                }    
+            }
+            return userId;
+        }
     }
 }
---
+++ DoConnect/TestConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataClient;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessLogic bl = new BusinessLogic();
+            bl.CreateUser();
+            Console.WriteLine("User Created.");
+            Console.ReadKey();
+        }
+    }
+}
