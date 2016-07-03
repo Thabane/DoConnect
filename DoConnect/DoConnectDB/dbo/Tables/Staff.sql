d35cdc402cfd045aeb478dab201bd12b84a7f2d6
Thabane Ndaba <skinnybonafide@yahoo.com>
2016-07-03 12:05:32 +0200

    Sunday Morning

---
+++ DoConnect/DoConnectDB/dbo/Tables/Staff.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Staff] (
+    [ID]            INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName]     NVARCHAR (50) NOT NULL,
+    [LastName]      NVARCHAR (50) NOT NULL,
+    [ID_Number]     NVARCHAR (50) NOT NULL,
+    [Gender]        CHAR (1)      NOT NULL,
+    [DOB]           DATE          NOT NULL,
+    [Phone]         NVARCHAR (50) NOT NULL,
+    [Employee_Type] NVARCHAR (50) NOT NULL,
+    [Practice_ID]   INT           NOT NULL,
+    [User_ID]       INT           NOT NULL,
+    CONSTRAINT [PK_Staff] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_Staff_Practice] FOREIGN KEY ([Practice_ID]) REFERENCES [dbo].[Practice] ([ID]),
+    CONSTRAINT [FK_Staff_Users] FOREIGN KEY ([User_ID]) REFERENCES [dbo].[Users] ([ID])
+);
+
