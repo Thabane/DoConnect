6e7c055602a4e7f610f535f4f2ab1364da69759f
THATO MADIHLABA <thato.madihlaba@live.com>
2016-07-04 10:05:32 +0200

    UI Design for the backend

---
+++ DoConnect/DocConnectApp/Admin/DoConnectAdmin/App/Controllers/PatientsController.js
@@ -0,0 +1,6 @@
+﻿app.controller("PatientsController", ["$scope", "PatientsService", "$interval",
+    function ($scope, PatientsService, $interval) {
+        
+        //Function Calls
+        //$interval($scope.GetStuff, 300000);
+    }]);
---
+++ DoConnect/DocConnectApp/Admin/DoConnectAdmin/App/app.js
@@ -0,0 +1,46 @@
+﻿var app = angular.module('app', ['ngRoute', 'ngLocationUpdate']);
+
+app.config(
+[
+    '$routeProvider', '$httpProvider', '$locationProvider',
+    function ($routeProvider, $httpProvider, $locationProvider) {
+
+        $routeProvider
+            .when("/Dashboard", {
+                templateUrl: "App/Views/Dashboard/dashboard.html",
+                controller: "DashboardController"
+            })
+            .when("/Patients", {
+                templateUrl: "App/Views/Patients/patients.html",
+                controller: "PatientsController"
+            })
+            .otherwise({
+                redirectTo: "/Dashboard"
+            });
+
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+
+        $httpProvider.interceptors.push('AuthHttpResponseInterceptor');
+    }
+]);
+
+
+app.run(
+    [
+    '$route', '$rootScope', '$location', function ($route, $rootScope, $location) {
+        var original = $location.path;
+        $location.path = function (path, reload) {
+            if (reload === false) {
+                var lastRoute = $route.current;
+                var un = $rootScope.$on('$locationChangeSuccess', function () {
+                    $route.current = lastRoute;
+                    un();
+                });
+            }
+            return original.apply($location, [path]);
+        };
+    }
+    ]);
