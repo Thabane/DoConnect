5b688d152bc24476c9e3c99956d4082892ede3db
Thabane Ndaba <thabanendaba@gmail.com>
2016-07-30 12:37:08 +0200

    Log Entry Done

---
+++ DoConnect/ObjectModel/Log.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectModel
+{
+    public class Log
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public string UserId { get; set; }
+    }
+}
