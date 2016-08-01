16141156c9844a58a1b0f4ca9d9d06734e03a18d
Thabane Ndaba <thabanendaba@gmail.com>
2016-08-01 16:18:48 +0200

    Get All Patients Done

--- DoConnect/ObjectModel/Patient.cs
+++ DoConnect/ObjectModel/Patient.cs
@@ -1,10 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace ObjectModel
-{    
+{
     public class Patient
-    {    
+    {
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -16,5 +17,24 @@ namespace ObjectModel
         public string Suburb { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
+
+        public Patient()
+        {
+        }
+
+        public Patient(SqlDataReader reader)
+        {
+            ID = reader.GetInt32(reader.GetOrdinal(""));
+            FirstName = reader.GetString(reader.GetOrdinal(""));
+            LastName = reader.GetString(reader.GetOrdinal(""));
+            ID_Number = reader.GetString(reader.GetOrdinal(""));
+            Gender = reader.GetString(reader.GetOrdinal(""));
+            DOB = reader.GetDateTime(reader.GetOrdinal(""));
+            Cell_Number = reader.GetString(reader.GetOrdinal(""));
+            Street_Address = reader.GetString(reader.GetOrdinal(""));
+            Suburb = reader.GetString(reader.GetOrdinal(""));
+            City = reader.GetString(reader.GetOrdinal(""));
+            Country = reader.GetString(reader.GetOrdinal(""));
+        }
     }
 }
