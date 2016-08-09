bde127dbc0e55eb5d8f87bf8fb19f19ff8fe2a0b
Thabane Ndaba <thabanendaba@gmail.com>
2016-08-09 20:00:25 +0200

    Add new Update practice  Sproc

--- DoConnect/DoConnectDB/dbo/Tables/Practice.sql
+++ DoConnect/DoConnectDB/dbo/Tables/Practice.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Practice] (
     [ID]             INT           IDENTITY (1, 1) NOT NULL,
     [Name]           NVARCHAR (50) NOT NULL,
-    [Cell_Number]    NVARCHAR (50) NOT NULL,
+    [Phone_Number]    NVARCHAR (50) NOT NULL,
     [Fax_Number]     NVARCHAR (50) NOT NULL,
     [Street_Address] NVARCHAR (50) NOT NULL,
     [Suburb]         NVARCHAR (50) NOT NULL,
