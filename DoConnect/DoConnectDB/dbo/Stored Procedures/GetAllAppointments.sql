4f02c4af2065027580dc87ef59638b46e4a34343
Thabane Ndaba <thabanendaba@gmail.com>
2016-08-09 20:45:24 +0200

    Get Sprocs Upload

---
+++ DoConnect/DoConnectDB/dbo/Stored Procedures/GetAllAppointments.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [GetAllAppointments] 
+
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+
+	SELECT        
+		[ID], 
+		[App_Status], 
+		[Date_Time], 
+		[Details], 
+		[Patient_ID], 
+		[Doctor_ID]
+	FROM            
+		[Appointments]
+
+END
+GO
---
+++ DoConnect/DoConnectDB/dbo/Stored Procedures/GetDocotorById.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [GetDocotorById] 
+		@UserId INT
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+
+	SELECT        
+		[FirstName], 
+		[LastName], 
+		[Gender], 
+		[ID], 
+		[Address], 
+		[Practice_ID], 
+		[User_ID], 
+		[Job_Title]
+	FROM            
+		[Doctors]
+	WHERE
+		[User_ID] = @UserId	
+
+END
+GO
