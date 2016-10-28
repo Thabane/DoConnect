82421376cca15412b2c5006435cf78ae18475c24
Josephine Chivinge <Josephine.chivinge@gmail.com>
2016-10-28 20:12:54 +0200

    CommandTimeout = 50;

--- DoConnect/DataClient/DataAccess.cs
+++ DoConnect/DataClient/DataAccess.cs
@@ -28,17 +28,20 @@ namespace DataClient
         /// <returns></returns>
         internal SqlDataReader ExecuteReader(string connectionString, string procName, List<SqlParameter> commandParameters)
         {
-            string connStr = connectionString;
-            var conn = new SqlConnection(connStr);
-            SqlCommand command = new SqlCommand(procName, conn);
+            
+                string connStr = connectionString;
+                var conn = new SqlConnection(connStr);
+                SqlCommand command = new SqlCommand(procName, conn);
 
-            if (commandParameters != null)
-                foreach (SqlParameter param in commandParameters)
-                    command.Parameters.Add(param);
+                if (commandParameters != null)
+                    foreach (SqlParameter param in commandParameters)
+                        command.Parameters.Add(param);
 
-            command.CommandType = CommandType.StoredProcedure;
-            conn.Open();
-            return command.ExecuteReader(CommandBehavior.CloseConnection);
+                command.CommandType = CommandType.StoredProcedure;
+                conn.Open();
+                command.CommandTimeout = 50;
+                return command.ExecuteReader(CommandBehavior.CloseConnection);
+            
         }
 
         /// <summary>
