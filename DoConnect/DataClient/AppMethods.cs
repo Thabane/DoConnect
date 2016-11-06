b53a22f43cb70d8c56b9a70ac4ddf65aee38db89
Thabane Ndaba <Thabane@users.noreply.github.com>
2016-11-06 16:11:40 +0200

    Add Service test (#26)
    * ServiceApp
    
    * Initial Push
    
    * Data Client
    
    * Test Fix
    
    * http post working
    
    * App - Patient Profile Update Service method
    
    * Return No Exception

---
+++ DoConnect/DataClient/AppMethods.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ObjectModel;
+
+namespace DataClient
+{
+    public class AppMethods
+    {
+        private DataAccess access;
+
+        private string Conn;
+
+        public AppMethods()
+        {
+            access = new DataAccess();
+            Conn = ConfigurationManager.ConnectionStrings["DB"].ConnectionString;
+        }
+
+        public string GetPatientByID(int id)
+        {
+            List<SqlParameter> _parameters = new List<SqlParameter>();
+            SqlParameter idParameter = new SqlParameter("@PatId", SqlDbType.Int);
+            idParameter.Value = id;
+            _parameters.Add(idParameter);
+            Patient patientInfo = new Patient();
+            using (var reader = access.ExecuteReader(Conn, "[GetPatById]", _parameters))
+            {
+                if (reader.Read())
+                {
+                    patientInfo = new Patient(reader);
+                    return JsonConvert.SerializeObject(patientInfo);
+                }
+            }
+            return null;
+        }
+        public bool UpdatePatient(int userId, string firstName, string lastName, string idNumber, char gender, string dob,string cellNumber,int medicalAidId, int doctorId, string streetaddress,string suburb, string city, string country)
+        {
+            List<SqlParameter> _parameters = new List<SqlParameter>();
+            SqlParameter userIdParameter = new SqlParameter("@User_ID", SqlDbType.Int);
+            userIdParameter.Value = userId;
+            _parameters.Add(userIdParameter);
+            //***************************
+            SqlParameter FirstNameParameter = new SqlParameter("@FirstName", SqlDbType.NVarChar);
+            FirstNameParameter.Value = firstName;
+            _parameters.Add(FirstNameParameter);
+            //***************************
+            SqlParameter LastNameParameter = new SqlParameter("@LastName", SqlDbType.NVarChar);
+            LastNameParameter.Value = lastName;
+            _parameters.Add(LastNameParameter);
+            //***************************
+            SqlParameter ID_NumberParameter = new SqlParameter("@ID_Number", SqlDbType.NVarChar);
+            ID_NumberParameter.Value = idNumber;
+            _parameters.Add(ID_NumberParameter);
+            //***************************
+            SqlParameter GenderParameter = new SqlParameter("@Gender", SqlDbType.Char);
+            GenderParameter.Value = gender;
+            _parameters.Add(GenderParameter);
+            //***************************
+            SqlParameter dobParameter = new SqlParameter("@DOB", SqlDbType.NVarChar);
+            dobParameter.Value = dob;
+            _parameters.Add(dobParameter);
+            //***************************
+            SqlParameter Cell_NumberParameter = new SqlParameter("@Cell_Number", SqlDbType.NVarChar);
+            Cell_NumberParameter.Value = cellNumber;
+            _parameters.Add(Cell_NumberParameter);
+            //***************************
+            SqlParameter Medical_Aid_IDParameter = new SqlParameter("@Medical_Aid_ID", SqlDbType.Int);
+            Medical_Aid_IDParameter.Value = medicalAidId;
+            _parameters.Add(Medical_Aid_IDParameter);
+            //***************************
+            SqlParameter Doctor_IDParameter = new SqlParameter("@Doctor_ID", SqlDbType.Int);
+            Doctor_IDParameter.Value = doctorId;
+            _parameters.Add(Doctor_IDParameter);
+            //***************************
+            SqlParameter Street_AddressParameter = new SqlParameter("@Street_Address", SqlDbType.NVarChar);
+            Street_AddressParameter.Value = streetaddress;
+            _parameters.Add(Street_AddressParameter);
+            //***************************
+            SqlParameter SuburbParameter = new SqlParameter("@Suburb", SqlDbType.NVarChar);
+            SuburbParameter.Value = suburb;
+            _parameters.Add(SuburbParameter);
+            //***************************
+            SqlParameter CityParameter = new SqlParameter("@City", SqlDbType.NVarChar);
+            CityParameter.Value = city;
+            _parameters.Add(CityParameter);
+            //***************************
+            SqlParameter CountryParameter = new SqlParameter("@Country", SqlDbType.NVarChar);
+            CountryParameter.Value = country;
+            _parameters.Add(CountryParameter);
+            //***************************
+
+            try
+            {
+                access.ExecuteNonQuery(Conn, _parameters, "[NewUpdatePatient]");
+                return true;
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ DoConnect/InfoServ/InfoServ.asmx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Remoting.Contexts;
+using System.Web;
+using System.Web.Script.Services;
+using System.Xml.Serialization;
+using System.Web.Services;
+using DataClient;
+using ObjectModel;
+
+namespace InfoServ
+{
+    /// <summary>
+    /// Summary description for InfoServ
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class InfoServ : WebService
+    {
+        /// <summary>
+        /// Gets the patient profile.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        [WebMethod]
+        public void GetPatientProfile(int id)
+        {
+                AppMethods am = new AppMethods();
+                string patient = am.GetPatientByID(id);
+                HttpContext.Current.Response.Write($"{patient}");        
+        }
+
+        /// <summary>
+        /// Updates the patient profile.
+        /// </summary>
+        /// <param name="userId">The user identifier.</param>
+        /// <param name="firstName">The first name.</param>
+        /// <param name="lastName">The last name.</param>
+        /// <param name="idNumber">The identifier number.</param>
+        /// <param name="gender">The gender.</param>
+        /// <param name="dob">The dob.</param>
+        /// <param name="cellNumber">The cell number.</param>
+        /// <param name="medicalAidId">The medical aid identifier.</param>
+        /// <param name="doctorId">The doctor identifier.</param>
+        /// <param name="streetaddress">The streetaddress.</param>
+        /// <param name="suburb">The suburb.</param>
+        /// <param name="city">The city.</param>
+        /// <param name="country">The country.</param>
+        [WebMethod]
+        public void UpdatePatientProfile(int userId, string firstName, string lastName, string idNumber, char gender, string dob, string cellNumber, int medicalAidId, int doctorId, string streetaddress, string suburb, string city, string country)
+        {
+            AppMethods am = new AppMethods();
+            bool patient = am.UpdatePatient(userId, firstName, lastName, idNumber, gender, dob, cellNumber, medicalAidId, doctorId, streetaddress, suburb, city, country);
+
+            if (patient)
+            {
+                HttpContext.Current.Response.Write("true");
+            }
+            else
+            {
+                HttpContext.Current.Response.Write("false");
+            }
+        }
+    }
+}
