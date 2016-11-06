92dc9cce2946288e7c98832d05879e3f017d4599
Thabane Ndaba <thabanendaba@gmail.com>
2016-11-06 14:44:44 +0200

    App - Patient Profile Update Service method

--- DoConnect/InfoServ/InfoServ.asmx.cs
+++ DoConnect/InfoServ/InfoServ.asmx.cs
@@ -24,12 +24,48 @@ namespace InfoServ
     [System.Web.Script.Services.ScriptService]
     public class InfoServ : WebService
     {
+        /// <summary>
+        /// Gets the patient profile.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
         [WebMethod]
         public void GetPatientProfile(int id)
         {
                 AppMethods am = new AppMethods();
                 string patient = am.GetPatientByID(id);
                 HttpContext.Current.Response.Write($"{patient}");        
+        }
+
+        /// <summary>
+        /// Updates the patient profile.
+        /// </summary>
+        /// <param name="userId">The user identifier.</param>
+        /// <param name="firstName">The first name.</param>
+        /// <param name="lastName">The last name.</param>
+        /// <param name="idNumber">The identifier number.</param>
+        /// <param name="gender">The gender.</param>
+        /// <param name="dob">The dob.</param>
+        /// <param name="cellNumber">The cell number.</param>
+        /// <param name="medicalAidId">The medical aid identifier.</param>
+        /// <param name="doctorId">The doctor identifier.</param>
+        /// <param name="streetaddress">The streetaddress.</param>
+        /// <param name="suburb">The suburb.</param>
+        /// <param name="city">The city.</param>
+        /// <param name="country">The country.</param>
+        [WebMethod]
+        public void UpdatePatientProfile(int userId, string firstName, string lastName, string idNumber, char gender, string dob, string cellNumber, int medicalAidId, int doctorId, string streetaddress, string suburb, string city, string country)
+        {
+            AppMethods am = new AppMethods();
+            bool patient = am.UpdatePatient(userId, firstName, lastName, idNumber, gender, dob, cellNumber, medicalAidId, doctorId, streetaddress, suburb, city, country);
+
+            if (patient)
+            {
+                HttpContext.Current.Response.Write("true");
+            }
+            else
+            {
+                HttpContext.Current.Response.Write("false");
+            }
         }
     }
 }
