f7633a2974cb045532a24873947f8c4df872f1bc
Josephine Chivinge <Josephine.chivinge@gmail.com>
2016-11-15 20:35:01 +0200

    Puxh work

---
+++ DoConnect/DataClient/LogFiles/LoginController.js
@@ -0,0 +1,26 @@
+﻿app.controller("LoginController", ["$scope", "LoginService", "$interval",
+    function ($scope, LoginService, $interval) {
+        
+        $scope.IsLogedIn = false;
+        angular.element("#wrapper").hide();
+        $scope.InvalidCredential = "null";
+        $scope.HereIAm = "HereIAm";
+        $scope.LoginData = {
+            Username: '',
+            Password: ''
+        };
+ 
+    
+        $scope.Login = function () { 
+                LoginService.GetUser($scope.LoginData).then(function (d) {
+                    if (d.data.Email != null) {
+                        $scope.IsLogedIn = true;
+                        angular.element("#wrapper").show();
+                        window.location.href = "Home/Index";
+                    }
+                    else {
+                        $scope.InvalidCredential = "Invalid Username or Password\nPlease enter a valid Username and Password";
+                    }
+                });
+        };
+}]);
